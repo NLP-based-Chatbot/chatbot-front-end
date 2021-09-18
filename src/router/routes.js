269c7cac8913b4849d7192cb0fb09e1860c358f5
YasithD <59837420+YasithD@users.noreply.github.com>
2021-09-18 19:04:43 +0530

    route changes added

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,12 +2,14 @@ import Home from '../views/home';
 import Product from '../views/product';
 import ContactUs from '../views/home/ContactUs';
 import PublicTransportation from './../views/chatbot/PublicTransportation';
+import Dashboard from './../views/dashboard/index';
 
 const routes = [
 	{ path: '/home', component: Home },
 	{ path: '/product', component: Product },
 	{ path: '/contactus', component: ContactUs },
-	{ path: '/chatbot/public-transportation', component: PublicTransportation }
+	{ path: '/chatbot/public-transportation', component: PublicTransportation },
+	{ path: '/admin/dashboard', component: Dashboard }
 ];
 
 export default routes;
