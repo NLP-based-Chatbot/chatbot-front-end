d402eecd400df4f853ecbaf79b433c721f604f35
YasithD <59837420+YasithD@users.noreply.github.com>
2021-09-29 06:22:31 +0530

    user management module
    account activation, forget password, reset password features added

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -3,15 +3,17 @@ import persistReducer from 'redux-persist/es/persistReducer';
 import storageSession from 'redux-persist/lib/storage/session';
 
 import AuthReducer from '../slices/auth';
+import EntitiesReducer from './entities';
 
 const persistConfig = {
 	key: 'root',
 	storage: storageSession,
-	whiteList: ['auth']
+	whiteList: ['auth', 'entities']
 };
 
 const rootReducer = combineReducers({
-	auth: AuthReducer
+	auth: AuthReducer,
+	entities: EntitiesReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
